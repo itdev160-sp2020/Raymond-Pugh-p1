58ee036cec2facc0e5dc433a91b870ecabeb7890
raypugh07 <pacmanmuzik88@gmail.com>
2020-04-22 12:20:07 -0500

    complete

--- js/index.js
+++ js/index.js
@@ -6,12 +6,16 @@ const siteContent = {
     },
     'topcontent':{
         "toptext":'Donec sit amet sapien egestas, vehicula enim nec, tristique turpis. Donec feugiat porttitor sem, vel porta justo ullamcorper ut. Nulla bibendum nisi vel leo pellentesque sagittis. Duis a fermentum leo. Aenean lacinia lorem eget pharetra suscipit. Vestibulum aliquet erat nisl, in bibendum ante pharetra eu. Duis venenatis blandit lacus, nec facilisis velit tincidunt a.'
+       , "img-src":'img/pexels-photo-4108271.jpeg'
     },
     'middlecontent':{
         'middletext':'Quisque congue sodales leo, et pretium mauris tincidunt in. Etiam sit amet maximus turpis. Nulla tristique metus in tellus posuere suscipit. In ultricies quam ex, a sollicitudin enim dapibus mattis. Duis accumsan tellus ac porta placerat. Donec porta malesuada quam, at pretium leo aliquam volutpat. Vivamus ac convallis ante, volutpat posuere odio. Nunc condimentum enim ut luctus gravida. Nunc vulputate varius consequat. Nunc semper viverra dolor, at accumsan ligula scelerisque non. Quisque eget mauris sed ante porta commodo eget sed tellus. Donec et sapien tortor. Mauris justo turpis, sodales vel euismod nec, commodo vitae sem.'
     },
     'bottomcontent':{
         'bottomtext': 'Suspendisse potenti. Fusce pretium porttitor mi ut euismod. Praesent aliquam mi urna, quis rhoncus quam hendrerit ac. Vivamus fermentum consequat consectetur. Duis ac est ac tortor tristique ultrices non et enim. Ut pretium ipsum magna, id egestas magna vulputate vel. Proin libero sapien, placerat vitae ipsum non, aliquet aliquam neque. Mauris sit amet tincidunt lorem. Suspendisse tempus venenatis orci vitae malesuada. Donec porta, ante in scelerisque semper, arcu massa fermentum nisl, vel varius felis ante sed justo. Quisque ornare erat vel sapien convallis, ut commodo est rutrum. In luctus magna non maximus interdum.'
+    },
+    'footer':{
+        'copyright': 'Copyright Project 1 2020'
     }
 }
 
@@ -52,3 +56,8 @@ let bottomValues = Object.values(siteContent['bottomcontent']);
 
 fill (bottomItems, bottomValues);
 
+let image = document.getElementById('image');
+image.setAttribute('src', siteContent['topcontent']['img-src']);
+
+let footer = document.querySelector('footer');
+footer.innerText = siteContent['footer']['copyright'];
